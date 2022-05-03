3c535596782adb1a2cc954513aa1c1cbe81d8b10
Marcos Iorio <marcossiorio@gmail.com>
2022-05-03 11:09:00 -0300

    added procfile

--- index.js
+++ index.js
@@ -98,7 +98,7 @@ catch (error) {
   
 (async () => {
     // Start your app
-    await app.start();
+    await app.start(process.env.PORT || 3000);
 
     console.log('⚡️ Bolt app is running!');
 })();
